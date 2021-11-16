3ae4fd278d1980126ec39c20b2cb0ce88f1ac2ef
merisen <contact@merisen.online>
2021-11-16 17:50:07 +0200

    Add implemented class

--- src/main/java/online/merisen/mulesoft/file/extension/ValidateExistFile.java
+++ src/main/java/online/merisen/mulesoft/file/extension/ValidateExistFile.java
@@ -1,5 +1,10 @@
 package online.merisen.mulesoft.file.extension;
 
-public class ValidateExistFile {
+import org.mule.extension.file.common.api.FileSystem;
 
+public class ValidateExistFile implements IValidateExistFile {
+	@Override
+	public void validateExistFile(FileSystem fileSystem, String directoryPath, String fileName) {
+		// TODO: ...
+	}
 }
